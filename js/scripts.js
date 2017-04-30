e7cfb978ae9849f1fbe0677b7c80270c47b7893e
Bram <bram.dejaegher@ugent.be>
2017-04-30 15:44:47 +0200

    restructuring + added modernizr

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var videoElement;
 var videoDiv;
 
 function eventWindowLoaded() {
-
+    window.alert("I'm loaded!")
     videoElement = document.createElement("video");
     videoDiv = document.createElement('div');
     document.body.appendChild(videoDiv);
@@ -16,9 +16,12 @@ function eventWindowLoaded() {
     }
     
     videoElement.addEventListener("canplaythrough",videoLoaded,false);
-    videoElement.setAttribute("src", "vids/animationPidpa.mp4");
+    videoElement.setAttribute("src", "vids/pidpaMasked.mp4");
     //videoElement.setAttribute("src", "muirbeach." + videoType); if everything works responsive videos
+}
 
+function canvasSupport () {
+     return Modernizr.canvas;
 }
 
 function videoLoaded(event) {
@@ -37,6 +40,34 @@ function supportedVideoFormat(video) {
        video.canPlayType("video/ogg") == "maybe") {
          returnExtension = "ogg";
    }
-
+    window.alert(returnExtension);
    return returnExtension;
+}
+
+function canvasApp() {
+   if (!canvasSupport()) {
+          return;
+        }
+
+function  drawScreen () {
+      //Background
+      context.fillStyle = '#ffffaa';
+      context.fillRect(0, 0, theCanvas.width, theCanvas.height);
+      //Box
+      context.strokeStyle = '#000000';
+      context.strokeRect(5,  5, theCanvas.width-10, theCanvas.height-10);
+      //video
+      context.drawImage(videoElement , 85, 30);
+   }
+
+   var theCanvas = document.getElementById("canvasOne");
+   var context = theCanvas.getContext("2d");
+   videoElement.play();
+
+   function gameLoop() {
+      window.setTimeout(gameLoop, 20);
+      drawScreen();
+   }
+
+   gameLoop();
 }
